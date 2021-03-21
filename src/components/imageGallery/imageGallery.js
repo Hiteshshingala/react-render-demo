a146439409c72cf72517087484cca3d25e375d27
Hiteshshingala <shingalahitesh2@gmail.com>
2021-03-21 16:10:38 +0530

    Initial Commit

---
+++ src/components/imageGallery/imageGallery.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+import { useHistory } from "react-router";
+
+function ImageGallery() {
+    const history = useHistory();
+    const imageData = 
+        [
+            {
+                id: 1,
+                name: 'img1',
+                img: 'https://via.placeholder.com/200/0000FF/808080'
+            },
+            {
+                id:2,
+                name: 'img2',
+                img: 'https://picsum.photos/200'
+            }
+        ];
+    
+    const [images, setImages] = useState(imageData)
+    const removeImage = (imageId) => {
+        const newImages = images.filter(img => img.id !== imageId )
+        setImages(newImages);
+    }
+
+    const prevStep = () => {
+        history.push("/message");
+    }
+    
+    return (
+        <div>
+            {images && images.length > 0 && images.map(element => {
+                return (
+                    <>
+                        <div className="image" key={element.id}>
+                            <img className="" src={element.img} />
+                            <button className="remove-img-btn" onClick={() => {removeImage(element.id)}} > X </button>    
+                        </div>
+                    </>
+                )
+            })}
+
+            <button onClick={() => {prevStep()}}>Previous Question  </button>
+
+        </div>
+    )
+}
+
+
+export default ImageGallery
+
